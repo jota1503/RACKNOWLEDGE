51cc8e377c122e35290829307fc22f3818305642
Richard <137957447+RicharDD160@users.noreply.github.com>
2023-07-20 19:12:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Racknowledge</title>
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <link rel="shortcut icon" href="img/Logo.ico" >
+</head>
+<body>
+    <p id="inicio" ></p>
+    <p id="logo"></p>
+    <p id="img"> <img src="img/Logo.png"></p>
+<p id="titulo">Te damos la bienvenida a Racknowledge</p>
+<p id="tex">Te damos la bienvenida a la pagina de educacion mas interactuva que podras conocer<br >esto con el fin de aprender de forma facil y manera correcta.</p>
+<button id="button1" href="inicio.html" ><a href="inicio.html">Inicia Sesion</a></button><p></p>
+<button id="button2" href="registro.html"><a href="registro.html">Registrate</a></button><p></p>
+</body>
+</html>
