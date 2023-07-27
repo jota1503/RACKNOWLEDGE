5849382df22ccd229af61564c4c9595acbc95ac0
jota1503 <137957281+jota1503@users.noreply.github.com>
2023-07-27 18:32:04 -0500

    Add files via upload

---
+++ RACKNOWLEDGE 1.3/js/cuestionario.js
@@ -0,0 +1,12 @@
+[
+    {
+        "Categoria": "Matematicas"
+        "Pregunta": "¿52 dividido por cuatro es igual a cuanto?1",
+        "Respuesta": "13",
+        "incorrecta1": "15",
+        "incorrecta2": "11",
+        "incorrecta3": "12",
+        "img": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F148%2F148785.png&tbnid=L27MHUVzQQrmCM&vet=12ahUKEwiwi5mKv6CAAxWemIkEHXWcD2oQMyhBegUIARCTAQ..i&imgrefurl=https%3A%2F%2Fwww.flaticon.es%2Ficono-gratis%2Fdivision_148785&docid=0M9MH86BeRAqQM&w=512&h=512&q=imagen%20de%20division&ved=2ahUKEwiwi5mKv6CAAxWemIkEHXWcD2oQMyhBegUIARCTAQ",
+        "object_fit": "cover"
+    }
+]
